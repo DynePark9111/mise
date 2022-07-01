768b1bafcfd930e049bfa23e5546fa75515a68f8
dynepark9111 <dynepark9111@gmail.com>
2022-07-02 07:09:01 +0900

    feat: BE setup. mise api.

--- NodeBack/src/index.ts
+++ NodeBack/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const app = express();
 const cors = require("cors");
+const miseRoutes = require("./routes/mise.routes");
 
 require("dotenv").config();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,8 @@ app.use(cors({ origin: ORIGIN, credentials: true }));
 app.get("/", (req, res) => {
   res.send("home");
 });
+
+app.use("/mise", miseRoutes);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
